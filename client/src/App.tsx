1d7774355ca3ffc436eabc2a25ab0484391da117
dudabergamo <dudabbm@gmail.com>
2025-12-09 16:59:39 -0300

    Add Login and Callback routes to Router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,11 +16,15 @@ import ManageKit from "@/pages/ManageKit";
 import Profile from "@/pages/Profile";
 import MemoryDetail from "./pages/MemoryDetail";
 import EditMemory from "./pages/EditMemory";
+import Login from "./pages/Login";
+import Callback from "./pages/Callback";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/callback" component={Callback} />
       <Route path="/onboarding" component={Onboarding} />
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/registrar-memoria" component={RegisterMemory} />
@@ -28,8 +32,8 @@ function Router() {
       <Route path="/minhas-memorias" component={MemoriesTimeline} />
       <Route path="/caixinhas" component={Caixinhas} />
       <Route path="/sortear-pergunta" component={SortearPergunta} />
-          <Route path="/gerenciar-kit" component={ManageKit} />
-          <Route path="/perfil" component={Profile} />
+      <Route path="/gerenciar-kit" component={ManageKit} />
+      <Route path="/perfil" component={Profile} />
       <Route path="/memoria/:id" component={MemoryDetail} />
       <Route path="/editar-memoria/:id" component={EditMemory} />
       <Route path="/404" component={NotFound} />
