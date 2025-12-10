6ec7506430a72b7c76a4d0acef027a865472e064
dudabergamo <dudabbm@gmail.com>
2025-12-09 21:12:09 -0300

    Add Auth0 configuration

---
+++ server/auth.ts
@@ -0,0 +1,18 @@
+import { auth, requiresAuth } from 'express-openid-connect';
+
+export const authConfig = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET!,
+  baseURL: process.env.AUTH0_BASE_URL!,
+  clientID: process.env.AUTH0_CLIENT_ID!,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL!,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+  routes: {
+    login: '/api/auth/login',
+    logout: '/api/auth/logout',
+    callback: '/api/auth/callback',
+  },
+};
+
+export { auth, requiresAuth };
